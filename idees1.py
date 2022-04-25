f071f628b621be756aef2e7607599e5879d4f0e8
hossein <hossein.soltanmanesh@gmail.com>
2022-04-25 09:38:30 +0430

    change speak

---
+++ idees1.py
@@ -0,0 +1,125 @@
+from cv2 import *
+from ecapture import ecapture as ec
+import speech_recognition as sr
+import pyttsx3
+import datetime
+from playsound import playsound
+from pygame import mixer
+import webbrowser
+import time
+
+print('Loading your AI personal assistant - G One')
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+engine.setProperty("rate" ,145)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def wishMe():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source,phrase_time_limit=10)
+
+        try:
+            statement=r.recognize_google(audio,language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Again")
+            return "None"
+        return statement
+speak('welcom')
+#speak("Loading your AI personal assistant G-One")
+wishMe()
+
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("say")
+        statement = takeCommand().lower()
+        if statement==0:
+            continue
+
+
+        if 'hello' in statement :
+            speak('hi sir how can i help you')
+        
+        if 'play' in statement:
+            mixer.init()
+            mixer.music.load("http://dl.vaiomusic.org/dl5/Music/2021/06/Alone Ft Pargar - Freak - 128.mp3")
+            mixer.music.play()  
+
+        
+        if 'wikipedia' in statement:
+            speak('Searching Wikipedia...')
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=10)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("youtube is open now")
+            time.sleep(5)
+
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Google chrome is open now")
+            time.sleep(5)
+
+        elif 'open insta' in statement:
+            webbrowser.open_new_tab("https://www.instagram.com")
+            speak("Google chrome is open now")
+            time.sleep(5)
+
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("Google Mail open now")
+            time.sleep(5)
+          
+        
+        elif 'search'  in statement:
+            statement = statement.replace("search", "")
+            webbrowser.open_new_tab(statement)
+            time.sleep(5)
+        
+        
+        elif 'open news' in statement:
+            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
+            speak('Here are some headlines from the Times of India,Happy reading')
+            time.sleep(6)
+
+        elif "open camera" in statement or "take a photo" in statement:
+            ec.capture(0,"robo camera","img.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
